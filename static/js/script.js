545771bfdca06c85da9e32a88040ea87b36f4acb
fetz236 <58368484+fetz236@users.noreply.github.com>
2021-06-20 21:29:52 +0300

    stand button makes dealer play
    stand button should play dealer automatically must be implemented

--- static/js/script.js
+++ static/js/script.js
@@ -164,26 +164,36 @@ let blackjackGame = {
     'you': {'scoreSpan' : '#your-blackjack-score', 'div': '#your-box', 'score': 0},
     'dealer': {'scoreSpan' : '#dealer-blackjack-score', 'div': '#dealer-box', 'score': 0},
     'cards' : ['2','3','4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'],
+    'cards_map' : {'2' : 2,'3':3,'4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'Q':10, 'J':10, 'A':[1,11]},
 };
 
 const user = blackjackGame['you'];
 const dealing = blackjackGame['dealer'];
 const hit_sound = new Audio('static/sounds/swish.m4a');
-const bust_sound = new Audio('static/sounds/aww.m4a');
+const bust_sound = new Audio('static/sounds/aww.mp3');
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackJackDeal);
 
 function blackjackHit(){
     let random_card = generateCard();
-    showCard(dealing, random_card);
+    showCard(user, random_card);
+    updateScore(user, random_card);
+
+    showScore(user);
+    
+     
 }
 
 function showCard(player, random_card){
-    let card_image = document.createElement('img');
-    card_image.src = 'static/images/${random_card}.png';
-    document.querySelector(player['div']).appendChild(card_image);
-    hit_sound.play();
+    if (player['score'] <=21){
+        let card_image = document.createElement('img');
+        card_image.src = 'static/images/' + random_card + '.png';
+        document.querySelector(player['div']).appendChild(card_image);
+        hit_sound.play();
+    }
+    
 }
 
 function blackJackDeal(){
@@ -192,12 +202,75 @@ function blackJackDeal(){
     for (let i =0; i < user_images.length; i++){
         user_images[i].remove();
     }
-    for (let i =0; i < dealer_images.length; i++){
-        dealer_images[i].remove();
+    for (let j =0; j < dealer_images.length; j++){
+        dealer_images[j].remove();
     }
+
+    user['score'] = 0;
+    dealing['score'] = 0;
+
+    document.querySelector('#your-blackjack-score').textContent = 0;
+    document.querySelector('#dealer-blackjack-score').textContent = 0;
+    document.querySelector('#your-blackjack-score').style.color = 'white';
+    document.querySelector('#dealer-blackjack-score').style.color = 'white';
 }
 
 function generateCard(){
     let random_card = Math.floor(Math.random() *13);
     return blackjackGame['cards'][random_card];
+}
+
+function updateScore(player, card){
+    
+    if(card === 'A'){
+        let card_value = player['score'] +blackjackGame['cards_map'][card][1];
+        if (card_value <=21){
+            player['score'] += blackjackGame['cards_map'][card][1];
+        }
+        else{
+            player['score'] += blackjackGame['cards_map'][card][0];
+        }
+    }
+    else{
+        player['score'] += blackjackGame['cards_map'][card];
+    }
+        
+}
+
+function showScore(player){
+    if (player['score'] <=21){
+        document.querySelector(player['scoreSpan']).textContent = player['score'];
+    }
+    else{
+        document.querySelector(player['scoreSpan']).textContent = 'BUST!';
+        document.querySelector(player['scoreSpan']).style.color = 'red';
+        
+    }
+    
+}
+function dealerLogic(){
+    let card = generateCard();
+
+    showCard(dealing, card);
+    updateScore(dealing, card);
+    showScore(dealing);
+}
+
+function computeWinner(){
+    let winner;
+    if(user['score'] <= 21){
+        if(user['score']> dealing['score'] || dealing['score']>21){
+            winner = user;
+        }
+        else if(user['score']< dealing['score']){
+            winner = dealing;
+        }
+        else if(user['score'] === dealing['score']){
+            //tie
+        }
+
+    }
+    else {
+        winner = dealer;
+    }
 }
